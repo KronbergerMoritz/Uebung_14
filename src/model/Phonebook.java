2a1a6825fe9bcff145c344070529f265dfa24997
Moritz Kronberger <moritzkronberger@aon.at>
2021-02-22 18:53:09 +0100

    Finished add, delete ,save, back, and next

--- src/model/Phonebook.java
+++ src/model/Phonebook.java
@@ -1,4 +1,85 @@
 package model;
 
-public class Phonebook {
+import java.util.ArrayList;
+
+public class Phonebook
+{
+    public Phonebook()
+    {
+        addNewPerson("Marcel Prilinger", "Eberstalzell", "0664 69686421");
+        addNewPerson("Moritz Kronberger", "Eberstalzell", "0677 62454541");
+        addNewPerson("Matteo Falkenber", "Wels", "0664 525174670");
+    }
+
+    public void addNewPerson(String name, String address, String phone)
+    {
+    Person person2 = new Person();
+    person2.setPhone(phone);
+    person2.setAddress(address);
+    person2.setName(name);
+
+    person.add(person2);
+    }
+
+    private ArrayList<Person> person = new ArrayList<>();
+
+
+
+    public void empty()
+    {
+        Person person1 = new Person();
+        person1.setAddress("");
+        person1.setName("");
+        person1.setPhone("");
+
+        person.add(person1);
+
+    }
+
+    public int size()
+    {
+        return person.size();
+    }
+
+    public void savePerson(int index, String name, String address, String phone)
+    {
+
+        person.get(index-1).setName(name);
+        person.get(index-1).setAddress(address);
+        person.get(index-1).setPhone(phone);
+    }
+
+
+    public Person getPerson(int index)
+    {
+        return person.get(index - 1);
+    }
+
+    public void deletePerson(int index)
+    {
+        person.remove(index - 1);
+    }
+
+
+
+
+
+    public void backPage()
+    {
+
+    }
+
+    public void saveCSV()
+    {
+
+    }
+
+    public void loadCSV()
+    {
+
+    }
+
+
+
+
 }
--- src/viewcontroller/Controller.java
+++ src/viewcontroller/Controller.java
@@ -1,4 +1,151 @@
 package viewcontroller;
 
-public class Controller {
-}
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import model.Person;
+import model.Phonebook;
+
+import java.io.IOException;
+
+public class Controller extends Phonebook{
+
+        private Phonebook phonebook = new Phonebook();
+        private int position = 1;
+
+        @FXML
+        private Label l_page;
+
+        @FXML
+        private TextField tf_name;
+
+        @FXML
+        private TextField tf_address;
+
+        @FXML
+        private TextField tf_phone;
+
+        @FXML
+        private Label l_error;
+
+        private void update()
+        {
+                l_page.setText(position + "/" + phonebook.size());
+                Person person = phonebook.getPerson(position);
+
+                tf_name.setText(person.getName());
+                tf_address.setText(person.getAddress());
+                tf_phone.setText(person.getPhone());
+        }
+
+
+        @FXML
+        public void initialize()
+        {
+                update();
+
+        }
+
+
+        @FXML
+        void add(MouseEvent event)
+        {
+                l_error.setText("");
+                phonebook.empty();
+                position = phonebook.size();
+                update();
+        }
+
+        @FXML
+        void back(MouseEvent event)
+        {
+                l_error.setText("");
+                if(position > 1)
+                {
+                        position--;
+                        update();
+                }
+                else
+                {
+                        l_error.setText("Keine Seite mehr Vorhanden!");
+                        l_error.setStyle("-fx-text-fill: red;");
+                        System.out.println("Keine Seite mehr Vorhanden!");
+                }
+        }
+
+        @FXML
+        void delete(MouseEvent event)
+        {
+                l_error.setText("");
+                if(phonebook.size() > 1)
+                {
+
+                        phonebook.deletePerson(position);
+                        if(position > phonebook.size())
+                        {
+                           position = phonebook.size();
+                        }
+                        update();
+
+
+                }
+                else
+                {
+                        tf_name.setText("");
+                        tf_phone.setText("");
+                        tf_address.setText("");
+                        l_error.setText("Alles bereits gelöscht");
+                        l_error.setStyle("-fx-text-fill: red;");
+                        System.out.println("nein");
+                }
+
+
+        }
+
+        @FXML
+        void loadcsv(MouseEvent event) {
+                l_error.setText("");
+
+        }
+
+        @FXML
+        void next(MouseEvent event)
+        {
+                l_error.setText("");
+                if(position < phonebook.size())
+                {
+                        position++;
+                        update();
+                }
+                else
+                {
+                        l_error.setText("Keine Seite mehr Vorhanden!");
+                        l_error.setStyle("-fx-text-fill: red;");
+                        System.out.println("Keine Seite mehr Vorhanden!");
+                }
+        }
+
+        @FXML
+        void save(MouseEvent event)
+        {
+                l_error.setText("");
+                String name = tf_name.getText();
+                String address = tf_address.getText();
+                String phone = tf_phone.getText();
+                phonebook.savePerson(position, name, address, phone);
+        }
+
+        @FXML
+        void savecsv(MouseEvent event) {
+                l_error.setText("");
+
+        }
+
+    }
+
